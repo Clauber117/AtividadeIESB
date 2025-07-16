53e519f5e922a0a488804ea105ead579f68872b1
Clauber Lima <149940593+Clauber117@users.noreply.github.com>
2025-07-16 20:37:13 -0300

    Add files via upload

--- SisCafe.py
+++ SisCafe.py
@@ -5,10 +5,38 @@
 
 import os
 
+cardapio={
+    1: {"prodnome": "Café Expresso",
+        "ingredientes": ["café moído", "água quente"],
+        "preco": 5.50},
+    2: {"prodnome": "Cappuccino",
+        "ingredientes": ["café expresso", "leite vaporizado", "espuma de leite", "canela"],
+        "preco": 8.90},
+    3: {"prodnome": "Latte",
+        "ingredientes": ["café expresso", "leite vaporizado"],
+        "preco": 9.50},
+    4: {"prodnome": "Mocha",
+        "ingredientes": ["café expresso", "leite", "chocolate", "chantilly"],
+        "preco": 12.00},
+    5: {"prodnome": "Chá Verde",
+        "ingredientes": ["folhas de chá verde", "água quente", "mel (opcional)"],
+        "preco": 7.00},
+    6: {"prodnome": "Macchiato",
+        "ingredientes": ["café expresso", "espuma de leite"],
+        "preco": 7.50}
+}
+
 def abertura():
     print("+----------------------------------------------------------------+")
     print("+                     Coffee Shops Tia Rosa                      +")
     print("+----------------------------------------------------------------+")
+    print("|                                                                |")
+    print("|                                                                |")
+    print("|                                                                |")
+    print("|                                                                |")
+    print("|                                                                |")
+    print("|                                                                |")
+    print("|                                                                |")
     print("|                                                                |")
     print("|                                                                |")
     print("+----------------------------------------------------------------+")
@@ -18,6 +46,16 @@ def abertura():
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
     abertura()
+
+    print("Produtos disponíveis na cafeteria:")
+    for id_produto, dados in cardapio.items():
+        print(f"\nID: {id_produto}")
+        print(f"Nome: {dados['prodnome']}")
+        print(f"Ingredientes: {', '.join(dados['ingredientes'])}")
+        print(f"Preço: R$ {dados['preco']:.2f}")
+
+    print("/n")
+    print(cardapio[3]["ingredientes"])
 
 if __name__== "__main__":
     main()
