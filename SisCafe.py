87a6464fc4041e8b6fa58d6e3a701fc874a2a15b
Clauber Lima <149940593+Clauber117@users.noreply.github.com>
2025-07-16 19:46:44 -0300

    Add files via upload

---
+++ SisCafe.py
@@ -0,0 +1,24 @@
+#Lógica Algoritmos e Programação de Computadores
+#Prof.: Francisco Lima
+#aluno: Clauber Araújo Lima
+#Id Aluno: 2586101058
+
+import os
+
+def abertura():
+    print("+----------------------------------------------------------------+")
+    print("+                     Coffee Shops Tia Rosa                      +")
+    print("+----------------------------------------------------------------+")
+    print("|                                                                |")
+    print("|                                                                |")
+    print("+----------------------------------------------------------------+")
+
+
+
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    abertura()
+
+if __name__== "__main__":
+    main()
+
