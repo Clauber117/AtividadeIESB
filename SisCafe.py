54659f6b18b84fc18013fbbf024e25b547c0e05a
Clauber Lima <149940593+Clauber117@users.noreply.github.com>
2025-07-16 21:44:38 -0300

    Add files via upload

--- SisCafe.py
+++ SisCafe.py
@@ -9,21 +9,30 @@ cardapio={
     1: {"prodnome": "Café Expresso",
         "ingredientes": ["café moído", "água quente"],
         "preco": 5.50},
-    2: {"prodnome": "Cappuccino",
+    2: {"prodnome": "Café Cappuccino",
         "ingredientes": ["café expresso", "leite vaporizado", "espuma de leite", "canela"],
         "preco": 8.90},
-    3: {"prodnome": "Latte",
+    3: {"prodnome": "Café com Leite",
         "ingredientes": ["café expresso", "leite vaporizado"],
         "preco": 9.50},
-    4: {"prodnome": "Mocha",
+    4: {"prodnome": "Café Mocha",
         "ingredientes": ["café expresso", "leite", "chocolate", "chantilly"],
         "preco": 12.00},
     5: {"prodnome": "Chá Verde",
         "ingredientes": ["folhas de chá verde", "água quente", "mel (opcional)"],
         "preco": 7.00},
-    6: {"prodnome": "Macchiato",
-        "ingredientes": ["café expresso", "espuma de leite"],
-        "preco": 7.50}
+    7: {"prodnome": "Toddy",
+        "ingredientes": ["Toddy", "leite"],
+        "preco": 4.00},
+    8: {"prodnome": "Nescau",
+        "ingredientes": ["Nescau", "leite"],
+        "preco": 4.00},
+    9: {"prodnome": "Pão de Queijo",
+        "ingredientes": ["Pão de Queijo"],
+        "preco": 3.50},
+   10: {"prodnome": "Coxinha",
+        "ingredientes": ["Coxinha"],
+        "preco": 4.50}
 }
 
 def abertura():
@@ -54,7 +63,7 @@ def main():
         print(f"Ingredientes: {', '.join(dados['ingredientes'])}")
         print(f"Preço: R$ {dados['preco']:.2f}")
 
-    print("/n")
+    print("\n")
     print(cardapio[3]["ingredientes"])
 
 if __name__== "__main__":
