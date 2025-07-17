86745d09c088f47417061a978b7234e623670c6a
Clauber Lima <149940593+Clauber117@users.noreply.github.com>
2025-07-17 18:07:23 -0300

    Update SisCafe.py

--- SisCafe.py
+++ SisCafe.py
@@ -2,38 +2,38 @@
 #Prof.: Francisco Lima
 #aluno: Clauber Araújo Lima
 #Id Aluno: 2586101058
-#Ver.:20250717.1429
+#Ver.: 20250717.1806
 
 import os
 import curses
 
 cardapio={
-    1: {"prodnome": "Café Expresso",
-        "ingredientes": ["café moído", "água quente"],
+    1: {"prodnome": "Cafe Expresso",
+        "ingredientes": "cafe moído agua quente",
         "preco": 5.50},
-    2: {"prodnome": "Café Cappuccino",
-        "ingredientes": ["café expresso", "leite vaporizado", "espuma de leite", "canela"],
+    2: {"prodnome": "Cafe Cappuccino",
+        "ingredientes": "cafe expresso, leite vaporizado, espuma de leite, canela",
         "preco": 8.90},
-    3: {"prodnome": "Café com Leite",
-        "ingredientes": ["café expresso", "leite vaporizado"],
+    3: {"prodnome": "Cafe com Leite",
+        "ingredientes": "cafe expresso, leite vaporizado",
         "preco": 9.50},
-    4: {"prodnome": "Café Mocha",
-        "ingredientes": ["café expresso", "leite", "chocolate", "chantilly"],
+    4: {"prodnome": "Cafe Mocha",
+        "ingredientes": "cafe expresso, leite, chocolate, chantilly",
         "preco": 12.00},
-    5: {"prodnome": "Chá Verde",
-        "ingredientes": ["folhas de chá verde", "água quente", "mel (opcional)"],
+    5: {"prodnome": "Cha Verde",
+        "ingredientes": "folhas de cha verde, água quente, mel (opcional)",
         "preco": 7.00},
     7: {"prodnome": "Toddy",
-        "ingredientes": ["Toddy", "leite"],
+        "ingredientes": "Toddy e leite",
         "preco": 4.00},
     8: {"prodnome": "Nescau",
-        "ingredientes": ["Nescau", "leite"],
+        "ingredientes": "Nescau e leite",
         "preco": 4.00},
-    9: {"prodnome": "Pão de Queijo",
-        "ingredientes": ["Pão de Queijo"],
+    9: {"prodnome": "Pao de Queijo",
+        "ingredientes": "Pao de Queijo",
         "preco": 3.50},
    10: {"prodnome": "Coxinha",
-        "ingredientes": ["Coxinha"],
+        "ingredientes": "Coxinha",
         "preco": 4.50}
 }
 
@@ -62,7 +62,7 @@ def ListarCardapio():
     for id_produto, dados in cardapio.items():
         print(f"\n{id_produto}",f"{dados['prodnome']}..............",f"Preço: R$ {dados['preco']:.2f}")
         #print(f"Nome: {dados['prodnome']}")
-        print(f"Ingredientes: {', '.join(dados['ingredientes'])}")
+        print(f"Ingredientes: {dados['ingredientes']}")
         #print(f"Preço: R$ {dados['preco']:.2f}")
 
 
