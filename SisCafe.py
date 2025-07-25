1cc04776b928f88c1352af94e70b9a5c4e90738c
Clauber Lima <149940593+Clauber117@users.noreply.github.com>
2025-07-25 10:23:12 -0300

    Add files via upload

--- SisCafe.py
+++ SisCafe.py
@@ -1,10 +1,10 @@
 #Lógica Algoritmos e Programação de Computadores
-#Prof.   : Francisco Lima
-#Id Al   : 2586101058
-#aluno   : Clauber Araújo Lima
-#Turma   : ADADS3A
-#Criacao : 20250716.1334
-__versao__ = 20250720.1142
+#Prof.  : Francisco Lima
+#Id Al  : 2586101058
+#aluno  : Clauber Araújo Lima
+#Turma  : EADADS3A
+#Criacao: 20250716.1334
+__versao__ = 20250725.1021
 
 import os, curses, json
 import funcoes
@@ -15,20 +15,26 @@ with open("cardapio.json",'r',encoding="utf-8") as f:
     cardapio=json.load(f)
     #funcoes.listarCardapio(cardapio)
 
+#Aqui carregamos os clientes
+with open("clientes.json",'r',encoding="utf-8") as f:
+    clientes=json.load(f)
+    #funcoes.listarCardapio(cardapio)
+
 def main():
-    while True:
+    opcao=1
+    while opcao!=0:
         opcao=0
         funcoes.MenuPrincipal()
-        opcao=int(input("Escolha a operação[1]:") or 1)
+        opcao=int(input(" Escolha a operação[0]:") or 0)
         #funcoes.listarCardapio(cardapio)
         if opcao==0:
             break
         elif opcao==1:
             funcoes.listarCardapio(cardapio)
+        elif opcao==3:
+            funcoes.listarClientes(clientes)
         print()
         input('Tecle ENTER para voltar')
-
-
 
 
 if __name__== "__main__":
--- funcoes.py
+++ funcoes.py
@@ -1,40 +1,54 @@
 # Funcoes
 #Criacao: 20250718.1124
-__versao__ = 20250720.1147
+__versao__ = 20250725.1021
 
 import os, json
 
-
 def MenuPrincipal():
     limparTela()
-    print("----------------------------------------------------------------")
-    print("                     Coffee Shops Tia Rosa                      ")
-    print("----------------------------------------------------------------")
+    print(" ----------------------------------------------------------------")
+    print("                      Coffee Shops Tia Rosa                      ")
+    print(" ----------------------------------------------------------------")
     print()
-    print("           1- Cardapio                                          ")
-    print("           2- Abrir Pedido                                      ")
-    print("           3- Clientes                                          ")
-    print("           0- Sair                                              ")
-    print()
-    print()
+    print("            1- Cardapio                                          ")
+    print("            2- Abrir Pedido                                      ")
+    print("            3- Clientes                                          ")
+    print("            0- Sair                                              ")
     print()
     print()
     print()
     print()
-    print("----------------------------------------------------------------")
+    print()
+    print()
+    print(" ----------------------------------------------------------------")
 
 def limparTela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def listarCardapio(cardapio):
     limparTela()
-    with open("cardapio.json",'r',encoding="utf-8") as f:
-        cardapio=json.load(f)
-    print("CARDAPIO:")
+    #with open("cardapio.json",'r',encoding="utf-8") as f:
+    #    cardapio=json.load(f)
+    print("----------------------------------------------------------------")
+    print(" \033[1;32m CARDAPIO:\033[0m")
+    print(" ----------------------------------------------------------------")
     for id_produto, dados in cardapio.items():
-        print(f"\n{id_produto}",f"{dados['prodnome']}..............",f"Preço: R$ {dados['preco']:.2f}")
+        nome=dados['prodnome']
+        pontinhos='.'*(30-len(nome))#
+        print(f"\n {id_produto} {nome}{pontinhos} Preço:\033[1;32m R$ {dados['preco']:.2f}\033[0m")
+        #print(f"\n{id_produto}",f"{dados['prodnome']}...........................",f"Preço: R$ {dados['preco']:.2f}")
         #print(f"Nome: {dados['prodnome']}")
-        print(f"Ingredientes: {dados['ingredientes']}")
+        print(f" Ingredientes: {dados['ingredientes']}")
         #print(f"Preço: R$ {dados['preco']:.2f}")
+
+def listarClientes(clientes):
+    limparTela()
+    #with open("cardapio.json",'r',encoding="utf-8") as f:
+    #    cardapio=json.load(f)
+    print(" ----------------------------------------------------------------")
+    print("\033[1;32m CADASTRO DE CLIENTES:\033[0m")
+    print(" ----------------------------------------------------------------")
+    for id_cliente, dados in sorted(clientes.items(),key=lambda item: item[1]['nome']):
+        print(f"\n {id_cliente} {dados['cpf']} {dados['nome']}\033[0m")
 
 
